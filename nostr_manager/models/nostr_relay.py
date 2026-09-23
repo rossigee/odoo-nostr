2f66dabb08d82c5bd5133be49c8c9cc2215e6cc6
Ross Golder <ross@golder.org>
2026-09-23 11:40:52 +0700

    fix: isort blank line between requests and odoo
    Treat odoo as first-party so isort 5 (pre-commit) and isort 9 (CI)
    agree on import sections.

--- nostr_manager/models/nostr_relay.py
+++ nostr_manager/models/nostr_relay.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
--- nostr_manager/tests/test_nostr_relay.py
+++ nostr_manager/tests/test_nostr_relay.py
@@ -3,6 +3,7 @@
 from unittest.mock import MagicMock, patch
 
 import requests
+
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
